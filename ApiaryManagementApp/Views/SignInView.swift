36fb83af4899c8f0d6edd1575a5430687087f7d5
Lavertis <29688908+Lavertis@users.noreply.github.com>
2022-05-12 23:27:56 +0200

    feat: add sign in

---
+++ ApiaryManagementApp/Views/SignInView.swift
@@ -0,0 +1,21 @@
+//
+//  SignInView.swift
+//  ApiaryManagementApp
+//
+//  Created by Rafał Kuźmiczuk on 12/05/2022.
+//  Copyright © 2022 Rafał Kuźmiczuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
