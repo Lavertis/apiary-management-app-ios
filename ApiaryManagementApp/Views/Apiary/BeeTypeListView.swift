78bc1b56d8bf72e5a294f86cd9f11195493dec97
Lavertis <29688908+Lavertis@users.noreply.github.com>
2022-05-13 16:57:51 +0200

    fix: seed bee types in on appear

--- ApiaryManagementApp/Views/Apiary/BeeTypeListView.swift
+++ ApiaryManagementApp/Views/Apiary/BeeTypeListView.swift
@@ -18,70 +18,44 @@ struct BeeTypeListView: View {
     @State private var offset: CGSize = .zero
     
     var body: some View {
-        VStack {
-            Button("Seed Bee Types") {
-                self.seedBeeTypes()
-            }
-            ZStack {
-                List {
-                    ForEach(beeTypes, id: \.self) { beeType in
-                        HStack {
-                            Image(beeType.img!)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 150)
-                                .gesture(LongPressGesture()
-                                    .onEnded { _ in
-                                        self.currentImg = beeType.img!
-                                        self.showImage = true
-                                })
-                            Text(beeType.name!)
-                        }
+        ZStack {
+            List {
+                ForEach(beeTypes, id: \.self) { beeType in
+                    HStack {
+                        Image(beeType.img!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                            .gesture(LongPressGesture()
+                                .onEnded { _ in
+                                    self.currentImg = beeType.img!
+                                    self.showImage = true
+                            })
+                        Text(beeType.name!)
                     }
                 }
-                if showImage {
-                    Image(self.currentImg)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: UIScreen.main.bounds.size.width * 0.8)
-                        .offset(self.offset)
-                        .gesture(DragGesture()
-                            .onEnded({ value in
-                                if abs(value.translation.width) > (UIScreen.main.bounds.size.width * 0.4) {
-                                    self.showImage = false
-                                }
-                                if abs(value.translation.height) > (UIScreen.main.bounds.size.height * 0.20) {
-                                    self.showImage = false
-                                }
-                                self.offset = .zero
-                            })
-                            .onChanged({ value in
-                                self.offset = value.translation
-                            }))
-                }
+            }
+            if showImage {
+                Image(self.currentImg)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: UIScreen.main.bounds.size.width * 0.8)
+                    .offset(self.offset)
+                    .gesture(DragGesture()
+                        .onEnded({ value in
+                            if abs(value.translation.width) > (UIScreen.main.bounds.size.width * 0.4) {
+                                self.showImage = false
+                            }
+                            if abs(value.translation.height) > (UIScreen.main.bounds.size.height * 0.20) {
+                                self.showImage = false
+                            }
+                            self.offset = .zero
+                        })
+                        .onChanged({ value in
+                            self.offset = value.translation
+                        }))
             }
         }.navigationBarTitle("Bee types")
-    }
-    
-    func seedBeeTypes() {
-        let beeType1 = BeeType(context: dbContext)
-        beeType1.name = "Bumblebee"
-        beeType1.img = "Bumblebee"
-        
-        let beeType2 = BeeType(context: dbContext)
-        beeType2.name = "Carpenter bee"
-        beeType2.img = "Carpenter_Bee"
-        
-        let beeType3 = BeeType(context: dbContext)
-        beeType3.name = "Honey bee"
-        beeType3.img = "Honey_Bee"
-        
-        do {
-            try dbContext.save()
-        } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
     }
 }
 
--- ApiaryManagementApp/Views/HomeView.swift
+++ ApiaryManagementApp/Views/HomeView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 
 
 struct HomeView: View {
+    @Environment(\.managedObjectContext) private var dbContext
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \BeeType.name, ascending: true)], animation: .default)
+    private var beeTypes: FetchedResults<BeeType>
+    
     @State var username: String? = "user"
     
     var body: some View {
@@ -52,6 +56,30 @@ struct HomeView: View {
                     }
                 }
             }.navigationBarTitle("Apiary management")
+        }.onAppear(perform: self.seedBeeTypes)
+    }
+    
+    private func seedBeeTypes() {
+        if beeTypes.count > 0 {
+            return
+        }
+        let beeType1 = BeeType(context: dbContext)
+        beeType1.name = "Bumblebee"
+        beeType1.img = "Bumblebee"
+        
+        let beeType2 = BeeType(context: dbContext)
+        beeType2.name = "Carpenter bee"
+        beeType2.img = "Carpenter_Bee"
+        
+        let beeType3 = BeeType(context: dbContext)
+        beeType3.name = "Honey bee"
+        beeType3.img = "Honey_Bee"
+        
+        do {
+            try dbContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
