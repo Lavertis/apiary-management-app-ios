b1a618b8f3c47015ce919649f46a2af877462fbe
Lavertis <29688908+Lavertis@users.noreply.github.com>
2022-05-13 19:19:09 +0200

    feat: add scale animation for dismissing bee images

--- ApiaryManagementApp/Views/Apiary/BeeTypeListView.swift
+++ ApiaryManagementApp/Views/Apiary/BeeTypeListView.swift
@@ -16,6 +16,7 @@ struct BeeTypeListView: View {
     @State private var showImage: Bool = false
     @State private var currentImg: String = ""
     @State private var offset: CGSize = .zero
+    @State private var scale: CGFloat = 1
     
     var body: some View {
         ZStack {
@@ -40,19 +41,26 @@ struct BeeTypeListView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: UIScreen.main.bounds.size.width * 0.8)
+                    .scaleEffect(self.scale)
                     .offset(self.offset)
                     .gesture(DragGesture()
                         .onEnded({ value in
                             if abs(value.translation.width) > (UIScreen.main.bounds.size.width * 0.4) {
                                 self.showImage = false
                             }
-                            if abs(value.translation.height) > (UIScreen.main.bounds.size.height * 0.20) {
+                            if abs(value.translation.height) > (UIScreen.main.bounds.size.height * 0.3) {
                                 self.showImage = false
                             }
                             self.offset = .zero
+                            self.scale = 1
                         })
                         .onChanged({ value in
                             self.offset = value.translation
+                            
+                            let widthOffsetPctg = abs(self.offset.width) / UIScreen.main.bounds.size.width
+                            let heightOffsetPctg = abs(self.offset.height) / UIScreen.main.bounds.size.height
+                            let maxVal = max(widthOffsetPctg, heightOffsetPctg)
+                            self.scale = 1 - 1.6 * maxVal
                         }))
             }
         }.navigationBarTitle("Bee types")
